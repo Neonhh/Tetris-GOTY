e17a1b185e9d15788d4ac1f88a9ce7c0e08e6a6a
Neonhh <thenjhu@gmail.com>
2020-01-16 19:21:49 -0430

    Movimiento sin aceleracion

---
+++ Scenes/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+export(float) var max_speed = 350.0
+
+var motion = Vector2.ZERO
+
+var velocity_direction = Vector2()
+
+func _ready() -> void:
+	$AnimatedSprite.play("default")
+
+func _physics_process(delta: float) -> void:
+	
+		var direction = get_direction()   #Obtiene vetores directores de norma 1
+		                                  #También revisa si las teclas están presionadas 
+	
+		if direction == Vector2.ZERO:           #Si no hay teclas presionadas, desacelera.
+			motion = Vector2.ZERO
+		else:                                  #Si las hay, acelera.
+			motion = direction * max_speed
+		
+		motion = move_and_slide(motion)
+	
+		#Obtén la dirección y velocidad del movimiento.
+func get_direction():
+	#Da 1 o -1 dependiendo de la dirección, 0 si la dirección no se puede definir (ninguna o ambas teclas presionadas)
+	var direction = Vector2.ZERO
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	#get_action_strength es lo mismo que un int(bool), devuelve 1 si la tecla está presionada, 0 si no. (teclas no análogas)
+	return direction.normalized()
