3f558a1c1c647fd9db72bbf9c043412427f36d14
Neonhh <thenjhu@gmail.com>
2020-01-16 21:17:18 -0430

    Bloque que cae versión 1

--- Scenes/Blocks/TetrisBlock1.gd
+++ Scenes/Blocks/TetrisBlock1.gd
@@ -4,10 +4,14 @@ export var speed = 200.0
 var direction = Vector2.DOWN
 var collision
 var stuck = false
-var glue
+var stuck_body
 
-func _process(delta):
-	
+func _physics_process(delta):
+
+		if stuck:
+			speed = stuck_body.max_speed
+			direction = stuck_body.direction
+		
 		collision = move_and_collide(direction * speed * delta)
 		
 		if collision: #Collision será null hasta que el objeto choque. En ese momento, se activa este if.
@@ -15,4 +19,7 @@ func _process(delta):
 	
 
 func collision_response(body):
+	if body.name == "Player":
+		stuck = true
+		stuck_body = body
 	
